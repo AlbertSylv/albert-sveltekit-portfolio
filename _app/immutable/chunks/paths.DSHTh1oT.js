a40458adebb141c01262a2265fcda1b55f6b60ee
AlbertSylv <AlbertSylv@users.noreply.github.com>
2024-06-28 18:49:57 +0000

    deploy: e2dbbdc7a2894a293d0b821f30637b4257335fca

--- _app/immutable/chunks/paths.DSHTh1oT.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_i265z3)==null?void 0:s.base)??"/albert-sveltekit-portfolio";var t;const a=((t=globalThis.__sveltekit_i265z3)==null?void 0:t.assets)??e;export{a,e as b};
