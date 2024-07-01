8fe80cd5917f01a19ad4a426277351243039fe92
AlbertSylv <AlbertSylv@users.noreply.github.com>
2024-07-01 12:54:52 +0000

    deploy: 5f57c0f60823ad5c1f13008909e995a198bf45cc

---
+++ _app/immutable/chunks/paths.BhEPAC7R.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_47zaog)==null?void 0:s.base)??"/albert-sveltekit-portfolio";var t;const e=((t=globalThis.__sveltekit_47zaog)==null?void 0:t.assets)??a;export{e as a,a as b};
