417ce7d76472e79151484c9bf775e0ed29761bf9
AlbertSylv <AlbertSylv@users.noreply.github.com>
2024-07-01 12:51:19 +0000

    deploy: 6c7b5222c4eabbcaf6f641a58b72cbf316f79210

---
+++ _app/immutable/chunks/paths.C-ujwvmy.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1j3q42g)==null?void 0:s.base)??"/albert-sveltekit-portfolio";var t;const a=((t=globalThis.__sveltekit_1j3q42g)==null?void 0:t.assets)??e;export{a,e as b};
